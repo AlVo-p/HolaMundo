cad7e2ff3f5a211512b0d951d07f4b44dfe035d7
AlVo-p <alesvan492@gmail.com>
2026-08-28 19:42:29 -0500

    actualizacion

--- src/M.java
+++ src/M.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class M {
     public static void main(String[] args) throws Exception {
-    
-        /*
+    /* 
+        
         private int edad=18;
         private double estatura=1.70;
         private char sexo='M';
@@ -17,7 +17,7 @@ public class M {
         double decimal = entero;
     }
         
-    Scanner sc = new Scanner(System.in)) {
+    Scanner sc = new Scanner(System.in) {
         System.out.print("Numero 1:");
         double a=sc.nextDouble();
 
@@ -46,11 +46,11 @@ public class M {
                 resultado=a/b;
             System.out.println(resultado);    
                 break;    
-        }
-    }
-}
-}
-*/ 
+        
+    
+
+
+
  
  Scanner sc = new Scanner(System.in);
 
@@ -63,6 +63,108 @@ public class M {
 
    }
    sc.close();
- }
+ 
+ 
+    for (int fila = 1; fila <=4; fila++) {
+        for (int col =1; col <=4; col++) {
+            System.out.print("*");
+    
+            }
+        System.out.println();
+        }
+    
+
+
+
+    for (int i = 1; i <= 10; i++ ) {
+        if (i == 5) break;
+        System.out.println(i);
+    }  
+        
+    
+
+    for (int i =1; i <=10; i++) {
+        if (i%2 ==0) continue;
+        System.out.println(i);
+    }
+
+
+
+    int contador=1;
+    while(contador <= 5){
+        System.out.println("Vuelta número" + contador);
+        contador++;
+    }
+
+            
+    Scanner sc=new Scanner(System.in);
+    int edad = -1;
+
+    while (edad <0 || edad > 120) {
+        System.out.print("ingresa tu edad (0-120): ");
+        edad = sc.nextInt();
+        if (edad <0 || edad > 120) {
+            System.out.println(("Edad inválida, intenta de nuevo. "));
+        }
+    }
+    System.out.println("Edad registrada: " + edad);
+    sc.close();
+    }}
+
+    Scanner sc=new Scanner(System.in);
+    int opcion;
+     do {
+        System.out.println("\n== MENÚ ==");
+        System.out.println("1. Salduda");
+        System.out.println("2. Despedirse");
+        System.out.println("3. Blablablabla");
+        System.out.println("0. Salir");
+        System.out.println("Poción: ");
+        opcion = sc.nextInt();
+        
+        switch (opcion) {
+            case 1: 
+            System.out.println("ola");
+                break;
+
+            case 2: 
+            System.out.println("Chao:(");
+                break;
+
+            case 3: 
+            System.out.println("Grosero");
+                break;
+        
+           }
+        }while(opcion!=0);
+        sc.close();
+     }}
+     
+    
+    int [] nota=new int[5]; {
+    nota[0] = 85;
+    nota[1] = 85;
+    nota[2] = 85;
+    nota[3] = 85;
+    nota[4] = 85;
+    System.out.println(nota [0]);
+    }}} 
+    */
+
+    Scanner sc=new Scanner(System.in);
+
+    System.out.println("¿Cuantos estudiantes hay?");
+    int n = sc.nextInt();
+    double[] notas = new double[n];
+    double suma = 0;
+
+    for 
+
+
+
+
+ 
+
+
 
 
